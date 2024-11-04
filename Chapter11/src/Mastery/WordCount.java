36e9ea41538481b5c4415aabe5146253a7ace23c
27237009 <27237009@L815WD220035.edu.cbe.ab.ca>
2024-11-04 12:21:20 -0700

    Updating

--- Chapter11/src/Mastery/WordCount.java
+++ Chapter11/src/Mastery/WordCount.java
@@ -40,11 +40,12 @@ public class WordCount {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		File textFile = new File("C:\\Users\\27237009\\git\\CS30P3F2024\\Chapter11\\src\\Mastery\\source.txt");
+		File textFile = new File("C:\\Users\\27237009\\git\\CS30P3F2024_7\\Chapter11\\src\\Mastery\\source.txt");
 		
 		System.out.println(textFile.exists() ? "Exists" : "Doesn't exist" + " | " + textFile.exists());
 		
@@ -52,7 +53,8 @@ public class WordCount {
 		
 		try 
 		{
-			Map<String, Integer> Words = new HashMap<>(); 
+			Map<String, Integer> Stricted_Words = new HashMap<>(); 
+			Map<String, Integer> Relaxed_Words = new HashMap<>(); 
 			
 			System.out.println("");
 			try (Scanner textFileReader = new Scanner(textFile)) 
@@ -60,39 +62,36 @@ public class WordCount {
 				while (textFileReader.hasNext()) 
 				{
 					String wordFound = textFileReader.next();
-					//System.out.println(wordFound);
+					wordFound.replace(" ", "");
 					
-					for (Map.Entry<String, Integer> Word : Words.entrySet()) 
+					if (Stricted_Words.containsKey(wordFound.toLowerCase())) 
 					{
-						if (textFileReader.equals(Word.getKey())) 
-						{
-							int Count = Word.getValue() + 1;
-							Word.setValue(Count);
-							
-							System.out.println(Word.getKey() + " | [" + Word.getValue() + "x]");
-						}
-					}
-					
-					if (Words.containsKey(textFileReader.next())) 
-					{
-						System.out.println("Added 1 to [" + textFileReader.next() + "]");
+						int Count = Stricted_Words.get(wordFound.toLowerCase()).intValue();
+						Stricted_Words.put(wordFound.toLowerCase(), Count + 1);
 						
-						int Count = Words.getOrDefault(textFileReader.next(), 1);
-						Words.put(textFileReader.next(), Count++);
-						
-						System.out.println(Words.get(textFileReader.next()) + " | [" + Words.get(textFileReader.next()).intValue() + "x]");
+						System.out.println("Stricted: " + wordFound + " (" + Stricted_Words.get(wordFound.toLowerCase()).intValue()  + ") | already exists");
 					}
 					else 
 					{
-						System.out.println("Created [" + textFileReader.next() + "]");
-						Words.put(textFileReader.next(), 1);
-					}	
+						Stricted_Words.put(wordFound.toLowerCase(), 0);
+						System.out.println("Stricted: " + wordFound + " Not inside hashmap adding.");
+					}
+					
+					if (Relaxed_Words.containsKey(wordFound)) 
+					{
+						int Count = Relaxed_Words.get(wordFound).intValue();
+						Relaxed_Words.put(wordFound, Count + 1);
+						
+						System.out.println("Relaxed: " + wordFound + " (" + Relaxed_Words.get(wordFound).intValue()  + ") | already exists");
+					}
+					else 
+					{
+						Relaxed_Words.put(wordFound, 0);
+						System.out.println("Relaxed: " + wordFound + " Not inside hashmap adding.");
+					}
+					
+					System.out.println("");
 				}
-			}
-			
-			for (Map.Entry<String, Integer> Word : Words.entrySet()) 
-			{
-				System.out.println(Word.getKey() + " | [" + Word.getValue() + "x]");
 			}
 		} 
 		catch (FileNotFoundException e) 
